1c49b40397d7ca25cca5aa3df89e8b99d4cefc73
Arun Gawri <luckygawri@gmail.com>
2020-08-08 00:53:27 +0530

    Installed Firebase and Hooked Up the app to fireabase project console

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import * as firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAK3wNPKFCmGbIrDpVDYb_ZEaeD8EbBgYQ",
+  authDomain: "restaurant-reservations-app.firebaseapp.com",
+  databaseURL: "https://restaurant-reservations-app.firebaseio.com",
+  projectId: "restaurant-reservations-app",
+  storageBucket: "restaurant-reservations-app.appspot.com",
+  messagingSenderId: "962725841954",
+  appId: "1:962725841954:web:312564dd297d8f71ee4e7b",
+  measurementId: "G-VW3DR1528M",
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
