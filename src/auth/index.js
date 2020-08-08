d56f3de124ba35d1409b8981c85f1bd9ce739558
Arun Gawri <luckygawri@gmail.com>
2020-08-08 13:58:48 +0530

    Util: Firebase Auth Wrapper Functions

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,4 +1,7 @@
+export { addAuthListener } from "./addAuthListener";
 export { CreateAccountPage } from "./CreateAccountPage";
 export { EmailConfirmationLanderPage } from "./EmailConfirmationLanderPage";
+export { signin } from "./signin";
 export { SignInForm } from "./SignInForm";
 export { SignInPage } from "./SignInPage";
+export { signOut } from "./signOut";
