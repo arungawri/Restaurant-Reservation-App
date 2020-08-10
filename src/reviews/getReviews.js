0232bd3ed2580e9ad07d9651860084fdaf7fc551
Arun Gawri <luckygawri@gmail.com>
2020-08-10 15:55:36 +0530

    Helper Functions: Review Wrapper Functions for fetching data

---
+++ src/reviews/getReviews.js
@@ -0,0 +1,26 @@
+import { firebase } from "firebase/app";
+import { mapAsync } from "../util";
+import { getUserInfo } from "../user";
+
+export const getReviews = async (restaurantId) => {
+  const querySnapshot = await firebase
+    .firestore()
+    .collections("reviews")
+    .where("restaurantId", "==", restaurantId)
+    .get();
+
+  const reviews = querySnapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+
+  const populatedReviews = await mapAsync(reviews, async (review) => {
+    const auther = await getUserInfo(review.userId);
+    return {
+      ...review,
+      auther,
+    };
+  });
+
+  return populatedReviews;
+};
