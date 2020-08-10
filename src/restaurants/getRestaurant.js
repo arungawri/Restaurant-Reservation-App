6bcf86cec49fca8225249b0ff6c8ed7b7e2a7cad
Arun Gawri <luckygawri@gmail.com>
2020-08-10 16:51:24 +0530

    Feature: subscribing reservation data to frontend

--- src/restaurants/getRestaurant.js
+++ src/restaurants/getRestaurant.js
@@ -1,15 +1,16 @@
 import { firebase } from "firebase/app";
 
-export const getRestaurant = aysnc id => {
-    const restaurantDoc = await firebase.firestore()
-        .collection("restaurant")
-        .doc(id)
-        .get();
+export const getRestaurant = async (id) => {
+  const restaurantDoc = await firebase
+    .firestore()
+    .collection("restaurant")
+    .doc(id)
+    .get();
 
-    const restaurant = restaurantDoc.data();
+  const restaurant = restaurantDoc.data();
 
-    return {
-        ...restaurant,
-        id
-    }
-}
+  return {
+    ...restaurant,
+    id,
+  };
+};
